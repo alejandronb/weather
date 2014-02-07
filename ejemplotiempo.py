2b92cc2c48a5fdb45a93fa814bb68e27c06be8cd
alejandronb <ale90nb@gmail.com>
2014-02-07 16:09:35 +0100

    Programa listo

--- ejemplotiempo.py
+++ ejemplotiempo.py
@@ -2,7 +2,6 @@
 import requests
 import json
 
-
 direccion = "http://api.openweathermap.org/data/2.5/weather?" #q=London,uk
 ciudades = {"1":"Almería","2":"Cádiz","3":"Córdoba","4":"Granada","5":"Huelva","6":"Jaén","7":"Málaga","8":"Sevilla"}
 
@@ -19,14 +18,10 @@ print """1. Almería
 consulta = raw_input("Introduce el número de la ciudad a consultar: ")
 
 if consulta in ciudades:
-	final = "%sq=%s,spain" % (direccion,ciudades[consulta])
-	print final
+	final = "%sq=%s,spain" % (direccion,ciudades[consulta]) #Esto construye la dirección de la API completa
 	peticion = requests.get(final)
-	print peticion
-#Trabajar con json.loads
+	json = json.loads(peticion.text)
+	temperatura = json["main"]["temp"] #Con esta línea cogemos lo que nos interesa del fichero json
+	celsius = round(temperatura - 273,1)
 
-
-
-#params={'q':%'s,spain' % capital}
-
-print "La temparatura actual de %s es grados centígrados" % ciudades[consulta]
+print "La temperatura actual de %s es %s grados centígrados" % (ciudades[consulta],celsius)
