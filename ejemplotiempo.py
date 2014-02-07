0f31e936a9fa7b45c7be18a25f2b6abeaeee0930
alejandronb <ale90nb@gmail.com>
2014-02-07 16:14:03 +0100

    Se ha depurado un poco el código

--- ejemplotiempo.py
+++ ejemplotiempo.py
@@ -17,11 +17,10 @@ print """1. Almería
 
 consulta = raw_input("Introduce el número de la ciudad a consultar: ")
 
-if consulta in ciudades:
-	final = "%sq=%s,spain" % (direccion,ciudades[consulta]) #Esto construye la dirección de la API completa
-	peticion = requests.get(final)
-	json = json.loads(peticion.text)
-	temperatura = json["main"]["temp"] #Con esta línea cogemos lo que nos interesa del fichero json
-	celsius = round(temperatura - 273,1)
+final = "%sq=%s,spain" % (direccion,ciudades[consulta]) #Esto construye la dirección de la API completa
+peticion = requests.get(final)
+json = json.loads(peticion.text)
+temperatura = json["main"]["temp"] #Con esta línea cogemos lo que nos interesa del fichero json
+celsius = round(temperatura - 273,1)
 
 print "La temperatura actual de %s es %s grados centígrados" % (ciudades[consulta],celsius)
